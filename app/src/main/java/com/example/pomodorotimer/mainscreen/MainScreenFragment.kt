5f8ac5cf7681535639dfb2bd471bd0bd81817331
Eric <erictomokigoto@gmail.com>
2020-07-06 20:00:40 -0700

    added options menu which links to the about fragment

--- app/src/main/java/com/example/pomodorotimer/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/mainscreen/MainScreenFragment.kt
@@ -5,13 +5,13 @@ import android.media.AudioManager
 import android.media.SoundPool
 import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.pomodorotimer.R
 import com.example.pomodorotimer.databinding.FragmentMainScreenBinding
 
@@ -45,10 +45,27 @@ class MainScreenFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        val binding: FragmentMainScreenBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_screen, container, false)
-        binding.startButton.setOnClickListener {findNavController().navigate(MainScreenFragmentDirections.actionMainScreenFragmentToTimerFragment())}
-
+        val binding: FragmentMainScreenBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_main_screen, container, false)
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(
+                MainScreenFragmentDirections.actionMainScreenFragmentToTimerFragment()
+            )
+        }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 
     companion object {
