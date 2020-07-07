23a436dbcb429d98687c19bf949fe59d6dd30777
Eric <erictomokigoto@gmail.com>
2020-07-06 19:59:58 -0700

    added data binding to about fragment

--- app/src/main/java/com/example/pomodorotimer/about/AboutFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/about/AboutFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.pomodorotimer.R
+import com.example.pomodorotimer.databinding.FragmentAboutBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +24,8 @@ class AboutFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +38,9 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+
+        return binding.root
     }
 
     companion object {
